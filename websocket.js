319462f15c4a287d84a98692988961faa28ca9a6
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-24 12:27:34 +0800

    initial room id setup

--- websocket.js
+++ websocket.js
@@ -3,22 +3,34 @@ const http = require('http')
 const server = http.createServer()
 const wss = new WebSocket.Server({ server })
 
-
+let users = []
+let id = 0
 wss.on('connection', (ws) => {
   console.log('Player connected')
-  
+
+  users.push({user: ws, room: undefined, id})
+  id++
+
   // Handle messages from players
   ws.on('message', (message) => {
-    // handle object conversion
-    const jsonString = message.toString('utf8')
-    const receivedObject = JSON.parse(jsonString)
+    console.log(message)
+    if(message.room) {
+        const index = users.findIndex(el => el.id == message.id)
+        users[index].room = message.room
+        console.log(users)
+    } else {
 
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(receivedObject))
-      }
-    });
-    console.log(`Received message: ${JSON.stringify(receivedObject)}`)
+      // handle object conversion
+      const jsonString = message.toString('utf8')
+      const receivedObject = JSON.parse(jsonString)
+
+      users.forEach(client => {
+        if (client !== ws && client.room == message.room) {
+          client.send(JSON.stringify(receivedObject))
+        }
+      });
+      console.log(`Received message: ${JSON.stringify(receivedObject)}`)
+    }
   })
 
   ws.on('close', () => {
